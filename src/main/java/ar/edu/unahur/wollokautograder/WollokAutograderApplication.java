bac74259600059c1149f071f19f807f58b98910a
Coniglio Hernan Marcelo <hernan.coniglio@tmoviles.com.ar>
2026-07-09 14:44:55 -0300

    chore: bootstrap Spring Boot project

---
+++ src/main/java/ar/edu/unahur/wollokautograder/WollokAutograderApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unahur.wollokautograder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WollokAutograderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WollokAutograderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unahur/wollokautograder/WollokAutograderApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unahur.wollokautograder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WollokAutograderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
